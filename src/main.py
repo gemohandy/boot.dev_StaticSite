fd90d65ae299c7d5225408f80593c22dc7a5d2ad
gemohandy <bennetteidsness@gmail.com>
2026-02-08 00:39:51 -0600

    Publish the Site

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from textnode import TextNode, TextType
 import os
 import shutil
+import sys
 from processor import markdown_to_html_node
 
 def main():
-    copyDirectory("static/", "public/")
-    generate_page_recursive("content/", "template.html", "public/")
+    basepath = "/"
+    if len(sys.argv) >= 2:
+        basepath = sys.argv[1]
+    copyDirectory("static/", "docs/")
+    generate_page_recursive(basepath, "content/", "template.html", "docs/")
 
 def copyDirectory(source, destination):
     if(os.path.exists(destination)):
@@ -30,7 +34,7 @@ def extract_title(markdown):
             return line[2:]
     raise Exception("No title found")
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
     sub_items = os.listdir(dir_path_content)
@@ -39,12 +43,12 @@ def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
         target_name = os.path.join(dest_dir_path, item)
         if os.path.isfile(full_name):
             if(full_name[-3:] == ".md"):
-                generate_page(full_name, template_path, target_name[:-3] + ".html")
+                generate_page(basepath, full_name, template_path, target_name[:-3] + ".html")
         else:
-            generate_page_recursive(full_name, template_path, target_name)
+            generate_page_recursive(basepath, full_name, template_path, target_name)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     file = open(from_path)
     markdown = file.read()
@@ -54,6 +58,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", inner_html)
+    template = template.replace("href=\"/", f"href=\"{basepath}")
+    template = template.replace("src=\"/", f"src=\"{basepath}")
     output_file = open(dest_path, "w")
     output_file.write(template)
 
